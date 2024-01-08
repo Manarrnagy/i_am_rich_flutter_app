0eb1fe6dec2298542053cb3a562371037a37473c
manarrnagy <manarrnagy@gmail.com>
2024-01-08 20:39:49 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[100],
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text("I Am Rich",textAlign: TextAlign.center,),
+        ),
+       body: Center(
+         child: Image.asset("assets/diamond.png"),
+       ),
+
+      )
+    );
+
+  }
+}
+
+
